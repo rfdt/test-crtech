0c60733dada024cbf762c424daa766e0927f21da
Ruslan Fedko <diamondmr84@gmail.com>
2023-09-16 21:19:57 +0300

    Installed Library

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {withNaming} from "@bem-react/classname";
+
+const cn = withNaming({e: '__', m: '_'}) // Модификатор
 
 function App() {
+
+  const menu = cn('Menu') // блок
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className={menu({collapsed: true})}>
+            // Пример элеменета с модификатором
+          <div className={menu('Top', {'collapsed': true})}></div>
+        </div>
     </div>
   );
 }
